6fff3ee79b53d972d2a55292de268656c2cc7a6c
JdM <julien@rmbi.ch>
2025-04-05 15:59:52 +0200

    add style to blog

--- blog/index.php
+++ blog/index.php
@@ -11,22 +11,25 @@ $articles = json_decode($data, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JdM's Blog</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <h1>JdM's Blog</h1>
         <h2><i>My ideas and thoughts</i></h2>
-        <br><br>
-        <h3>Articles</h3>
-        <ul>
-            <?php
-                $i = 0;
-                foreach ($articles as $article) {
-                    echo "<li><a href=\"read.php?i=$i\">{$article['title']}</a></li>";
-                    $i++;
-                }
-            ?>
+
     </header>
+    <br><br>
+    <h3>Articles :</h3>
+    <ul>
+        <?php
+            $i = 0;
+            foreach ($articles as $article) {
+                echo "<li><a href=\"read.php?i=$i\">{$article['title']}</a></li>";
+                $i++;
+            }
+        ?>
+    </ul>
     
 </body>
 </html>
--- blog/read.php
+++ blog/read.php
@@ -13,6 +13,7 @@ $content = file_get_contents("articles/" . $article["html"]);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $article["title"] ?></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
